308b5caa03a59693e8a7fda19f455f19aedaf1e0
hding2196 <hding2196@gmail.com>
2022-04-25 13:04:58 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Pages from "./pages/Pages";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Pages/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Popular from '../components/Popular'
+import Veggie from '../components/Veggie'
+
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <Popular/>
+      <Veggie/>
+    </div>
+  )
+}
+
+export default Home
